768dbbfcf39a43d59cb22eafe27c359917ce7fa7
Gilles Hoarau <unicolored@gmail.com>
2012-06-17 16:32:43 +0200

    Suppression du thème enfant pour le moment.

--- wp-content/themes/boot-gilleshoarau/attachment.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?>
-
-		<?php get_template_part( 'loop', 'attachment' ); ?>
-
-<?php get_footer();?>
--- wp-content/themes/boot-gilleshoarau/content--carousel-copy2.php
+++
@@ -1,8 +0,0 @@
-<a href='<?php echo the_permalink() ?>' title='<?php the_title(); ?>'>
-<?php
-global $url;
-//echo $url;
-?>
-<b class="img" style="height:570px; width:850px; display:block;
-background:url(<?php echo $url?>) center center no-repeat;"></b>
-</a>
