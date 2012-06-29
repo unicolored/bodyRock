8395414378c83eccf51d0e1630a421be41d81d6b
Gilles Hoarau <unicolored@gmail.com>
2012-06-29 09:20:03 +0200

    Update

--- wp-content/themes/bodyrock/searchform.php
+++ wp-content/themes/bodyrock/searchform.php
@@ -7,6 +7,7 @@
  * @since Twenty Eleven 1.0
  */
 ?>
+<h3 class="widget-title">Rechercher</h3>
 	<form method="get" id="searchform" class="form-search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 		<input type="text" class="field input-medium search-query" name="s" id="s" placeholder="<?php esc_attr_e( 'Rechercher', 'bodyrock' ); ?>" />
 		<input type="submit" class="btn submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Rechercher', 'bodyrock' ); ?>" />
