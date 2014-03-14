f116f15fa8b0a80188b04cb7f218363ba4e50ea9
Gilles Hoarau <unicolored@gmail.com>
2014-03-15 00:51:35 +0100

    Rename des fichiers admin.php qui creent conflits

--- assets/inc/wp-extend/types/evenements/main.php
+++ assets/inc/wp-extend/types/evenements/main.php
@@ -4,7 +4,7 @@ require_once('type.php');
 require_once('taxonomy.php'); 
 require_once('metabox.php'); 
 
-require 'admin.php'; 
+require 'admintype.php'; 
 require_once('display.php'); 
 
 ?>
--- assets/inc/wp-extend/types/lieux/main.php
+++ assets/inc/wp-extend/types/lieux/main.php
@@ -2,7 +2,7 @@
 require_once('type.php'); 
 require_once('taxonomy.php'); 
 require_once('metabox.php'); 
-require_once('admin.php'); 
+require_once('admintype.php'); 
 require_once('display.php'); 
 
 ?>
