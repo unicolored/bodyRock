cdeb0c57dd893c38d302ca193f515bf424e0874e
Gilles W <unicolored@gmail.com>
2014-11-06 22:47:54 +0100

    Mise aux normes des fichiers

--- single.php
+++ single.php
@@ -1,6 +1,5 @@
 <?php
-get_header();
-
+// Ajouter des scripts supplémentaires ci-dessous
 if (get_post_format()=='video') {
 	// Spécifique aux formats video
 	global $urlfinale, $videoType, $videoCode;
@@ -10,31 +9,58 @@ if (get_post_format()=='video') {
 	
 	$urlfinale = CDN_PATH.'images/videos/'.$videoType.'/'.$videoCode.br_getImgVideoSize('thumbnail').'.jpg';
 }
-/************** HTML START **************/
 
-echo a('section.content');
-	echo '<hr class="margin">';
-	if ( have_posts() ) :
-	
-		while ( have_posts() ) :
-		
-			the_post();
-			setPostViews(get_the_ID());	
-			get_template_part(TPL_SINGULAR_PATH.'single', get_post_format());
-			
-		endwhile;
-		
-	else:
-	
-		echo '<p>'.__('Sorry, no posts matched your criteria.').'</p>';
-		
-	endif;
-	
-echo '</section>';
-	
-get_template_part(TPL_SIDEBAR_PATH.'sidebar','left');
+// HEADER
+get_header();
 
-get_template_part(TPL_SIDEBAR_PATH.'sidebar','right');
-	
-get_footer();
+the_post();
 ?>
+<section class="br_bonjour page">
+<?php // Placer le code Html ci-dessous ?>
+
+
+    <section>
+        <div class="container">
+              <h1><?php the_title() ?></h1>
+              <?php echo the_content(); ?>
+        </div>
+    </section>
+    
+    <section class="article">
+          <div class="container">
+                <?php
+                  if (have_posts()) :
+            
+                        while (have_posts()) :
+            
+                              the_post();
+                              setPostViews(get_the_ID());
+                              get_template_part(TPL_SINGULAR_PATH . 'single', get_post_format());
+            
+                        endwhile;
+                  else :
+            
+                        echo '
+            		<p>
+            			' . __('Sorry, no posts matched your criteria.') . '
+            		</p>';
+            
+                  endif;
+                  ?>
+            </div>
+    </section>
+    
+    <aside>
+          <div class="container">
+                  <?php
+                  get_template_part(TPL_SIDEBAR_PATH.'sidebar','left');
+                  get_template_part(TPL_SIDEBAR_PATH.'sidebar','right');
+                  ?>
+            </div>
+      </aside>
+    </aside>
+
+
+<?php // Placer le code Html ci-dessus ?>
+</section>
+<?php get_footer();?>
