f1d87d8cf0dceddbc4c420396f88b051c3e504f9
Gilles Hoarau <unicolored@gmail.com>
2014-12-11 17:54:01 +0100

    Ajout de l'icone Atom

--- dist/bodyrock/index.php
+++ dist/bodyrock/index.php
@@ -10,26 +10,20 @@ get_header(); ?>
 
       <section class="intro">
             <div class="jumbotron">
-		    <h1>Index.</h1>
+              <h1>BodyRock Demo</h1>
 		</div>
 	</section>
-	
-      <section class="intro">
-            <div class="well">
-                <h1>Articles de la catégorie "featured"</h1>
-            </div>
-      </section>
-    
-    
+
+
     <section>
         <div class="container">
-            <?php
+            <?php /*
             if ( get_category_by_slug('featured') != false ) {
                   $args='posts_per_page=12&post_status=publish&cat='.get_category_by_slug('featured')->term_id;
-                  
+
                   // The Query
                   $the_query = new WP_Query( $args );
-                  
+
                   // The Loop
                   if ( $the_query->have_posts() ) {
                       while ( $the_query->have_posts() ) {
@@ -41,21 +35,20 @@ get_header(); ?>
                           <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
                           <strong>Warning!</strong> Better check yourself, you're not looking too good.
                         </div>
-                        <?php    
+                        <?php
                   }
-                  /* Restore original Post Data */
                   wp_reset_postdata();
             } else { ?>
                   <div class="alert alert-warning alert-dismissible" role="alert">
                         <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
                         <strong><?php br_Icon('warning'); ?> Aucun article trouvé</strong> Placez des articles dans la catégorie "featured" pour qu'ils apparaissent ici.
                   </div>
-                  <?php    
-            }
+                  <?php
+            }*/
             ?>
         </div>
     </section>
-    
+
     <section class="cadre">
         <div class="container">
             <?php // get_template_part('tpl/article','lorem'); ?>
